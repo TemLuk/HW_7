8c1beae9a695d8076f57a97dfe788b55f3d9030b
TemLuk <98478422+TemLuk@users.noreply.github.com>
2022-02-23 18:49:02 +0200

    Update artem_bakirov_task_1_hw_7.py

--- artem_bakirov_task_1_hw_7.py
+++ artem_bakirov_task_1_hw_7.py
@@ -1,7 +1,7 @@
 import math
 x = float(input("Введите сторону квадрата:"))
 def squre_operation(x: float):
-    return 'Периметр = ' + str(x * 4), 'Диагональ = ' + str(round(x * math.sqrt(2), 1)), 'Площадь = ' + str(x * x)
+    return 'Периметр = ' + str(x * 4), 'Диагональ = ' + str(round(x * math.sqrt(2), 1)), 'Площадь = ' + str(round(x * x, 1))
 
 
 print(squre_operation(x))
